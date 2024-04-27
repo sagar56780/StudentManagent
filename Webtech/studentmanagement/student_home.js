b7a94343206d33c6a64cf2d9df57da730e617892
Your Name <you@example.com>
2024-04-27 15:28:10 -0700

    city-guide-demo

--- Webtech/studentmanagement/student_home.js
+++ Webtech/studentmanagement/student_home.js
@@ -2,14 +2,19 @@ var nav_button=document.getElementById("nav-button");
 console.log(nav_button);
 var burger=document.getElementById("burger");
 var boolean=true;
+var nav_bar=document.getElementById("nav-bar");
 
 nav_button.addEventListener("click",()=>{
+  
     if(boolean==true){
         burger.style.display="block";
+   
         boolean=false;
     }
     else{
         burger.style.display="none";
+    
+
         boolean=true;
 
     }

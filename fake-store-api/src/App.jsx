1e936bfcdcb26c9db2c4859aa96b92b7333a1cab
Your Name <you@example.com>
2024-05-15 19:02:36 -0500

    first commit

--- fake-store-api/src/App.jsx
+++ fake-store-api/src/App.jsx
@@ -1,13 +1,47 @@
-import { computeHeadingLevel } from "@testing-library/react";
-import PrductCard from "./ProductCard";
+import React, { useState, useEffect } from 'react';
+import PrductCard from './ProductCard';
 
-const App=()=>{
-    fetch('https://fakestoreapi.com/products')
-            .then(res=>res.json())
-            .then(json=>json.map((val,ind)=>{
-                console.log(val.category);
-                return <><p>{val.value}</p></>
-            }))
-    
-}
+const App = () => {
+    var [data, setData] = useState([]);
+
+  
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products/')
+            .then(res => res.json())
+            .then(json => setData(json))
+            .catch(error => console.error('Error fetching data:', error));
+         
+     
+    }, []); // Empty dependency array ensures the effect runs only once after the initial render
+   const  allItems=data;
+
+    let handelMens=()=>{
+        setData(data.filter(data =>  data.category=="men's clothing"))
+      
+
+    } 
+
+   
+    let handelAll=()=>{
+      setData(allItems)
+      
+      
+    }
+
+
+    return (
+      <>
+    <center> <nav><button className='nav-bar' onClick={handelAll}>view All items</button><button className='nav-bar' onClick={handelMens}>men's clothing</button> <button className='nav-bar'>women's clothing</button> <button className='nav-bar'>jewelery</button> <button className='nav-bar' >Electronics </button></nav></center> 
+      
+      <div className='mainContainer'>
+
+      
+          {data.map((product, index) => (
+               <PrductCard key={index} rating={product.rating.rate}   image={product.image} price={product.price} category={product.category}  title={product.title} description={product.description} ></PrductCard>
+            ))}
+      </div>
+      </>
+    );
+};
+
 export default App;
--- fake-store-api/src/ProductCard.jsx
+++ fake-store-api/src/ProductCard.jsx
@@ -1,17 +1,29 @@
+import { useState } from "react";
 
 
-const PrductCard=()=>{
+const PrductCard=(data)=>{
+
+
+    
     return(<>   
-            <div className="container">
-            <img src="" alt="" />
-             <p>Title: </p>
-             <p>Price: </p>
-             <p>Description:</p>
-             <p>Category:</p>
-             <p>Rating:</p>
-
-            </div>
-
+        
+      
+                <div className="container" id="con" >
+                    <img src={data.image} alt="" />
+                     <p className="title">{data.title}</p>
+                     <p className="price">${data.price}</p>
+                     <p className="category">{data.category}</p>
+                     <p className="rating">{data.rating}</p>
+                      <details >
+                        <summary>Description</summary>
+                        <dd>{data.description}</dd>
+                      </details>
+                    
+                </div>
+         
+          
+         
+       
     </>)
 }
 export default PrductCard;
